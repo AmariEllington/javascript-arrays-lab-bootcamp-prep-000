b5cff439e0e58abc83ff9a0d97523a0b8634e2cc
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-07 20:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob')
+}
+
+function destructivelyRemoveLastKitten(name){
+   kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var kitten = ['Milo', 'Otis', 'Garfield']
+  var kittenNew = ['Broom']
+  var animals = kitten.concat(kittenNew)
+}
